c6908aeb6cfdbd099da7bba6d03512499c57c302
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2026-01-02 15:51:17 +0000

    refactor(ch01): add output

--- src/ch01_intro/binary_search.py
+++ src/ch01_intro/binary_search.py
@@ -1,19 +1,6 @@
 from typing import List, Optional
 
 def binary_search(list_data: List[int], item: int) -> Optional[int]:
-    """
-    Performs a binary search on a sorted list to find the index of a specific item.
-    
-    Time Complexity: O(log n)
-    Space Complexity: O(1)
-
-    Args:
-        list_data (List[int]): A sorted list of integers.
-        item (int): The item to search for.
-
-    Returns:
-        Optional[int]: The index of the item if found, otherwise None.
-    """
     low = 0
     high = len(list_data) - 1
 
@@ -29,3 +16,17 @@ def binary_search(list_data: List[int], item: int) -> Optional[int]:
             low = mid + 1
             
     return None
+
+if __name__ == "__main__":
+    my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    target = 13
+    
+    result = binary_search(my_list, target)
+    
+    print(f"List: {my_list}")
+    print(f"Target: {target}")
+    
+    if result is not None:
+        print(f"Item found at index: {result}")
+    else:
+        print("Item not found.")
